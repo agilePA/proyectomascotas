f4a9795879741f63388f028d34dd6c028709aa83
cr0wg4n <dark.hunter.crow@gmail.com>
2018-10-05 14:08:21 -0400

    mauricio final

---
+++ PetsAPI/PetsAPI/Migrations/20181005125609_Mascotas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PetsAPI.Migrations
+{
+    public partial class Mascotas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mascotas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(maxLength: 50, nullable: false),
+                    Raza = table.Column<string>(maxLength: 50, nullable: false),
+                    Especie = table.Column<string>(maxLength: 50, nullable: false),
+                    Edad = table.Column<string>(nullable: false),
+                    Sexo = table.Column<string>(nullable: false),
+                    Enfermedades = table.Column<string>(nullable: false),
+                    Descripcion = table.Column<string>(nullable: true),
+                    Procedencia = table.Column<string>(nullable: true),
+                    Imagen = table.Column<string>(nullable: true),
+                    Estado = table.Column<bool>(nullable: false),
+                    Created_at = table.Column<DateTime>(nullable: false),
+                    Updated_at = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mascotas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mascotas");
+        }
+    }
+}
---
+++ PetsAPI/PetsAPI/Models/Mascota.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetsAPI.Models
+{
+    public class Mascota
+    {
+        [Key]
+        [Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Nombre no puede tener mas de 50 caracteres.")]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Raza no puede tener mas de 50 caracteres.")]
+        public string Raza { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Especie no puede tener mas de 50 caracteres.")]
+        public string Especie { get; set; }
+        [Required]
+        public string Edad { get; set; }
+        [Required]
+        public string Sexo { get; set; }
+        [Required]
+        public string Enfermedades { get; set; }
+        public string Descripcion { get; set; }
+        public string Procedencia { get; set; }
+        public string Imagen { get; set; }
+        public bool Estado { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+    }
+}
